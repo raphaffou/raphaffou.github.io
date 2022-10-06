08c7139287f6a4f6a8a0f073de443986cad220f1
raphael.thieffry <52051654+raphaffou@users.noreply.github.com>
2022-10-06 20:48:40 +0200

    first commit

---
+++ script/noRefreshLinks.js
@@ -0,0 +1,10 @@
+const collection = document.getElementsByClassName("link");
+var content = document.getElementById("main");
+function noref() {
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].onclick = function() {
+            content.innerHTML = "<div w3-include-html=\""+collection[i].getAttribute("src")+"\"></div>";
+        }
+    }
+}
+setInterval(noref, 500);
